b602d03f9293dbe2658bbdbe20e24ce1f1c4c272
bharathiraja-developer <bharathiraja798@gmail.com>
2024-04-04 20:10:49 +0530

    Frontend

--- src/components/Bookings.jsx
+++ src/components/Bookings.jsx
@@ -4,6 +4,14 @@ import "../styles/Bookings.css";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 
+function NoBooking() {
+  return (
+    <div className="col-12 d-flex justify-content-center">
+      <h2>No Bookings</h2>
+    </div>
+  );
+}
+
 function Bookings() {
   const { register, setRegister } = useContext(userContext);
   const [value, setValue] = useState([]);
@@ -58,31 +66,35 @@ function Bookings() {
         </div>
       </nav>
       <div className="container-fluid mt-0 mb-0 ms-0 me-0 body">
-        {value.map((item) => {
-          return (
-            <div className="row" key={Math.random() * 10}>
-              <div className="col-3 d-flex justify-content-start">
-                <img
-                  style={{ width: "100%", height: "100%", objectFit: "fill" }}
-                  src={item.image}
-                ></img>
-              </div>
-              <div className="col-4 d-flex justify-content-start">
-                <div>
-                  <h4 className="ms-2 mt-4">{item.modelName}</h4>
-                  <p className="ms-2 mt-2">{item.details.brand}</p>
+        {value.length > 0 ? (
+          value.map((item) => {
+            return (
+              <div className="row" key={Math.random() * 10}>
+                <div className="col-3 d-flex justify-content-start">
+                  <img
+                    style={{ width: "100%", height: "100%", objectFit: "fill" }}
+                    src={item.image}
+                  ></img>
                 </div>
+                <div className="col-4 d-flex justify-content-start">
+                  <div>
+                    <h4 className="ms-2 mt-4">{item.modelName}</h4>
+                    <p className="ms-2 mt-2">{item.details.brand}</p>
+                  </div>
+                </div>
+                <div className="col-5">
+                  <p className="d-flex justify-content-end pe-4 mt-4">
+                    Price : Rs.{item.price[1]}/day
+                  </p>
+                  <div className="d-flex justify-content-end pe-4 mt-4"></div>
+                </div>
+                <hr className="mt-3"></hr>
               </div>
-              <div className="col-5">
-                <p className="d-flex justify-content-end pe-4 mt-4">
-                  Price : Rs.{item.price[1]}/day
-                </p>
-                <div className="d-flex justify-content-end pe-4 mt-4"></div>
-              </div>
-              <hr className="mt-3"></hr>
-            </div>
-          );
-        })}
+            );
+          })
+        ) : (
+          <NoBooking />
+        )}
       </div>
     </div>
   );
