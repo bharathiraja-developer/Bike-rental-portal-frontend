d5c21485042fc85ed128f592f67c04f7114bf99a
bharathiraja-developer <bharathiraja798@gmail.com>
2024-04-04 19:47:23 +0530

    Frontend

--- src/components/Bookings.jsx
+++ src/components/Bookings.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { userContext } from "../App";
 import "../styles/Bookings.css";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Bookings() {
   const { register, setRegister } = useContext(userContext);
@@ -25,32 +25,65 @@ function Bookings() {
       setValue(res.data.bookings);
     });
   return (
-    <div className="container-fluid mt-0 mb-0 ms-0 me-0 body">
-      {value.map((item) => {
-        return (
-          <div className="row" key={Math.random() * 10}>
-            <div className="col-3 d-flex justify-content-start">
-              <img
-                style={{ width: "100%", height: "100%", objectFit: "fill" }}
-                src={item.image}
-              ></img>
-            </div>
-            <div className="col-4 d-flex justify-content-start">
-              <div>
-                <h4 className="ms-2 mt-4">{item.modelName}</h4>
-                <p className="ms-2 mt-2">{item.details.brand}</p>
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid px-5">
+          <a className="navbar-brand" href="#!">
+            Welcome {register.name} !
+          </a>
+          <form className="d-flex">
+            <Link to="/Home" className="btn btn-outline-dark me-2">
+              Home
+              <span className="badge bg-dark text-white ms-1 rounded-pill"></span>
+            </Link>
+            <Link
+              to="/Signin"
+              onClick={() => {
+                sessionStorage.clear();
+                setregister({
+                  token: "",
+                  username: "",
+                  name: "",
+                  mobile: "",
+                  address: "",
+                  password: "",
+                });
+              }}
+              className="btn btn-outline-dark me-2"
+            >
+              Logout
+              <span className="badge bg-dark text-white ms-1 rounded-pill"></span>
+            </Link>
+          </form>
+        </div>
+      </nav>
+      <div className="container-fluid mt-0 mb-0 ms-0 me-0 body">
+        {value.map((item) => {
+          return (
+            <div className="row" key={Math.random() * 10}>
+              <div className="col-3 d-flex justify-content-start">
+                <img
+                  style={{ width: "100%", height: "100%", objectFit: "fill" }}
+                  src={item.image}
+                ></img>
               </div>
+              <div className="col-4 d-flex justify-content-start">
+                <div>
+                  <h4 className="ms-2 mt-4">{item.modelName}</h4>
+                  <p className="ms-2 mt-2">{item.details.brand}</p>
+                </div>
+              </div>
+              <div className="col-5">
+                <p className="d-flex justify-content-end pe-4 mt-4">
+                  Price : Rs.{item.price[1]}/day
+                </p>
+                <div className="d-flex justify-content-end pe-4 mt-4"></div>
+              </div>
+              <hr className="mt-3"></hr>
             </div>
-            <div className="col-5">
-              <p className="d-flex justify-content-end pe-4 mt-4">
-                Price : Rs.{item.price[1]}/day
-              </p>
-              <div className="d-flex justify-content-end pe-4 mt-4"></div>
-            </div>
-            <hr className="mt-3"></hr>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 }
